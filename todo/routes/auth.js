1d6181ab93d807fb0ac746a1ee31a29bebc3654f
ARIHANT <157610621+ARIHANT218@users.noreply.github.com>
2025-07-28 21:46:10 +0530

    Delete todo directory

--- todo/routes/auth.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-const router = express.Router();
-import authController from '../controller/authController.js';
-router.post('/register', authController.registerController);
-
-router.post('/login', authController.loginController);
-
-export default router;
